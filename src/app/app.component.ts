9d255315e1387b386bd59774f120cbcb12536e0c
Akash Patel <akash.patel@vortilla.com>
2020-01-10 14:35:05 +0530

    Added Header and Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angulardesign';
+  faShoppingBasket = faShoppingCart;
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+    console.log("event::", window.pageYOffset);
+    let element = document.getElementById('navbar');
+    if (window.pageYOffset >= 50) {
+      element.classList.add('sticky');
+    } else {
+      element.classList.remove('sticky');
+    }
+  }
 }
