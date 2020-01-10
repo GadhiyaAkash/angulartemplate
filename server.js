4fa0470f3d2e0c73a53dca2f0456aa8842048e03
Akash Patel <akash.patel@vortilla.com>
2020-01-10 15:03:34 +0530

    Fixed typo

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angulartemplate'));
+app.use(express.static(__dirname + '/dist/angulardesign'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angulartemplate/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angulardesign/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
